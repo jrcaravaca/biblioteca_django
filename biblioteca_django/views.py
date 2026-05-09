c4f22fad37875eb96c0912f6618aa72d91387f0c
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2026-05-09 08:32:40 +0200

    Añado input para buscador de momento sin funcion, solo para diseño

--- biblioteca_django/views.py
+++ biblioteca_django/views.py
@@ -17,7 +17,7 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context =  super().get_context_data(**kwargs)
-        books = Book.objects.all()
+        books = Book.objects.order_by('-id')[:6]
         context['books'] = books
         
         return context
